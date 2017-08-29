6855eeec9fc99f2c16f6796d42106cfebdc438ea
dimitar <dimitar@doonloo.com>
2017-08-29 20:00:45 +0300

    📝 Add types tests for availability

---
+++ __tests__/index.ts
@@ -0,0 +1,9 @@
+import {Game, GameUnit} from "../src/";
+
+test('Should have Game type available', () => {
+  expect(Game).toBeTruthy();
+});
+
+test('Should have GameUnit type available', () => {
+  expect(GameUnit).toBeTruthy();
+});
