a78b46622181fde914d71fdaba19577b998feb48
dimitar <dimitar@doonloo.com>
2017-09-14 00:00:03 +0300

    📝 Add tests for type existence

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,4 +1,12 @@
-import { Game, GameUnit } from '../src/';
+import { AuthCode, AuthResult, Game, GameUnit, UnitParameter } from '../src/';
+
+test('Should have AuthCode type available', () => {
+  expect(AuthCode).toBeTruthy();
+});
+
+test('Should have AuthResult type available', () => {
+  expect(AuthResult).toBeTruthy();
+});
 
 test('Should have Game type available', () => {
   expect(Game).toBeTruthy();
@@ -6,4 +14,8 @@ test('Should have Game type available', () => {
 
 test('Should have GameUnit type available', () => {
   expect(GameUnit).toBeTruthy();
+});
+
+test('Should have UnitParameter type available', () => {
+  expect(UnitParameter).toBeTruthy();
 });
